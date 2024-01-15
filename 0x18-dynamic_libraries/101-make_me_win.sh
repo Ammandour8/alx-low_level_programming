e5c0e3f46f1e4f67f0693c2e60e299bff92e6a77
Ammandour8 <amrmandour51@gmail.com>
2024-01-15 11:32:37 -0800

    hack fot jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P https://github.com/Ammandour8/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
